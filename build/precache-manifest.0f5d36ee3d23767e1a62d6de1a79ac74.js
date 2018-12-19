0ba8501d31a60bc593a6a8f931a0b614fd21a66b
zhangzejun3 <zzj2472@163.com>
2018-12-19 21:12:57 +0800

    build

---
+++ build/precache-manifest.0f5d36ee3d23767e1a62d6de1a79ac74.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d3b9c50724f60d0f923",
+    "url": "/static/js/main.5d3b9c50.chunk.js"
+  },
+  {
+    "revision": "3c52a61c72e19f37399c",
+    "url": "/static/js/1.3c52a61c.chunk.js"
+  },
+  {
+    "revision": "06f286d9abddf8ca9ed2d67c4facadad",
+    "url": "/index.html"
+  }
+];
